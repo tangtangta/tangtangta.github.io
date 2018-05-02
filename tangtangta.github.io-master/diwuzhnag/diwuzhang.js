a6560cc1eed5637d31bd57bebd46a16a789f734e
tangtangta <38269882+tangtangta@users.noreply.github.com>
2018-05-02 10:52:17 +0800

    Add files via upload

---
+++ tangtangta.github.io-master/diwuzhnag/diwuzhang.js
@@ -0,0 +1,14 @@
+window.onload=function(){
+  if(!document.getElementsByTagName) return false;
+  var lnks=document.getElementsByTagName("a");
+  for(var i=0;i<lnks.length;i++){
+    if(lnks[i].getAttribute("class")=="popup"){
+      lnks[i].onlick=function(){
+        popUp(this.getAttribute("href"));
+        return false;
+        }
+      }
+    }
+  }
+function popUp(winURL) {
+window.open(winUR "popup"£©£»}
